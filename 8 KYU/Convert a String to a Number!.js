18e78482fb21f22165d9b524217dfc255cd2daac
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-04-25 07:21:52 -0500

    Initial commit

---
+++ 8 KYU/Convert a String to a Number!.js
@@ -0,0 +1,13 @@
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+var stringToNumber = function (str) {
+  return parseInt(str);
+};
