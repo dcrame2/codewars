6dc6a123f17b215731ded758e2bdc51eb7277f0b
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-05-22 08:29:07 -0500

    add break camelcase and counting sheep codewars

---
+++ 6 KYU/Break camelCase.js
@@ -0,0 +1,16 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+  string = string.split("").map(function (letter) {
+    if (letter === letter.toUpperCase()) {
+      letter = " " + letter;
+    }
+    return letter;
+  });
+  return string.join("");
+}
