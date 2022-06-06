4ee685f75a6347cca40ef67447e64bf724cd18c7
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-06-06 07:08:20 -0500

    Create Mumbling.js
    Using multiple chained methods together (split, map, join, slice)

---
+++ 7 KYU/Mumbling.js
@@ -0,0 +1,17 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+function accum(s) {
+  return s
+    .toLowerCase()
+    .split("")
+    .map((letter, i) => {
+      let newStr = letter.repeat(i + 1);
+      return newStr.slice(0, 1).toUpperCase() + newStr.slice(1);
+    })
+    .join("-");
+}
