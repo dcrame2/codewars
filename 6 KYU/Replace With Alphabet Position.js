2a5ac361759fd4bf8c85d296873ac798030a7988
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-07-16 13:37:50 -0500

    update

--- 6 KYU/Replace With Alphabet Position.js
+++ 6 KYU/Replace With Alphabet Position.js
@@ -5,7 +5,7 @@
 // If anything in the text isn't a letter, ignore it and don't return it.
 
 // "a" = 1, "b" = 2, etc.
-
+//update
 // Example
 // alphabetPosition("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
