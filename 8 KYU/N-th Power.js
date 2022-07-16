3c99f8feaef311e9eb5a84cbc92c3f8fda5bb267
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-07-16 13:28:43 -0500

    update

--- 8 KYU/N-th Power.js
+++ 8 KYU/N-th Power.js
@@ -6,6 +6,7 @@
 
 // array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
 // array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+
 function index(array, n) {
   return typeof array[n] === "undefined" ? -1 : array[n] ** n;
 }
