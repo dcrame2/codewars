ba485eeaaddebc99cd7c41f40431a94f692f09eb
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-07-16 13:39:03 -0500

    update

--- 6 KYU/Your order, please.js
+++ 6 KYU/Your order, please.js
@@ -3,7 +3,7 @@
 // Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
 
 // If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
-
+//update
 // Examples
 // "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
 // "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
