fb67bcb8b1a7814dcd3137ed3638e9e9288684c3
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-07-24 12:00:00 -0500

    update

--- 8 KYU/Even or Odd.js
+++ 8 KYU/Even or Odd.js
@@ -1,5 +1,5 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-//updat
+//update
 function even_or_odd(number) {
   return number % 2 === 0 ? "Even" : "Odd";
 }
