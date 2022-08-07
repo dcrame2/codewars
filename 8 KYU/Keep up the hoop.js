9eb74e62cf88bbc7ad43e8d6ffad04c14d594464
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-08-07 13:25:23 -0500

    update

--- 8 KYU/Keep up the hoop.js
+++ 8 KYU/Keep up the hoop.js
@@ -4,7 +4,9 @@
 
 // If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
-
-function hoopCount (n) {
-    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"   
- }
+//adding new function
+function hoopCount(n) {
+  return n >= 10
+    ? "Great, now move on to tricks"
+    : "Keep at it until you get it";
+}
