ef3d39c542329392c1054f83195844062296f063
Dylan Cramer <dcrame2@gmail.com>
2022-11-30 11:38:29 -0600

    filter method & includes

---
+++ 7 KYU/Filter out the geese.js
@@ -0,0 +1,7 @@
+//
+
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter((bird) => !geese.includes(bird));
+  // return an array containing all of the strings in the input array except those that match strings in geese
+}
